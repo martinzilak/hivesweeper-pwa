9ff7236e57bc8d1dc9be60718ab81c96a3fd37b6
Martin Zilak <martin.zilak@mailbox.org>
2023-09-03 13:08:56 +0200

    Responsive page.

--- src/modules/web/components/Footer/index.tsx
+++ src/modules/web/components/Footer/index.tsx
@@ -1,22 +1,23 @@
 import { FC } from 'react';
 import styles from './Footer.module.scss';
+import classNames from 'classnames';
 
 const Footer: FC = () => {
   return (
     <div className={styles.footer}>
-      <div className={styles.footerItem}>
+      <div className={classNames(styles.footerItem, styles.createdBy)}>
         <span>Created by</span>
         <span>Martin Zilak</span>
       </div>
 
-      <div className={styles.footerItem}>
+      <div className={classNames(styles.footerItem, styles.contactMe)}>
         <a href="mailto:martin@zilak.sk?subject=Hivesweeper" className={styles.mailLink}>
           Contact me
         </a>
         <span>via e-mail</span>
       </div>
 
-      <div className={styles.footerItem}>
+      <div className={classNames(styles.footerItem, styles.downloadAt)}>
         <a href="https://apps.apple.com/us/app/hivesweeper/id1581731994" className={styles.downloadButton}></a>
       </div>
     </div>
